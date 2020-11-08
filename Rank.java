9b1c94209f3e2f5fddd7513d10e3f4434b6eaf64
Chao Yu <18301086@bjtu.edu.cn>
2020-11-08 16:14:36 +0800

    rgb排序

---
+++ Rank.java
@@ -0,0 +1,45 @@
+package rank;
+import java.util.Arrays;
+
+public class Rank {
+	public static String[] reRank(String[] input) {
+		return new String[0];
+	}
+	
+	public static String[] reRankTwoPointers(String[] input) {
+		if(input == null || input.length == 0) {
+			return new String[0];
+		}
+		int i = 0;
+		int left = 0;
+		int right = input.length - 1;
+		while(i <= right) {
+			if(input[i].equals("r")) {
+				String t1 = input[i];
+				input[i] = input[left];
+				input[left] = t1;
+				i++;
+				left++;
+			}
+			else if(input[i].equals("g")) {
+				i++;
+			}
+			else if(input[i].equals("b")) {
+				String t2 = input[i];
+				input[i] = input[right];
+				input[right] = t2;
+				right--;
+			}
+		}
+		return input;
+	}
+	
+	public static void main(String[] args) {
+		String[] input = new String[] {"r", "b", "g", "r", "b"};
+		Arrays.asList(input);
+		String[] result;
+		result = reRankTwoPointers(input);
+		for(String str : result)
+			System.out.print(str);
+	}
+}
